2d7695771e23a165384c40fa7021d0c2bd790ee0
Stanislav Chernenko <64794434+stanis-chernes@users.noreply.github.com>
2023-07-04 10:54:14 +0200

    feat: added erc-1155-reward warper interface

---
+++ contracts/universus/IERC1155RewardWarperForUniversus.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@iqprotocol/iq-space-protocol/contracts/accounting/Accounts.sol";
+
+interface IERC1155RewardWarperForUnivsus {
+
+}
