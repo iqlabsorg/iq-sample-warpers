a54c4fd5da47cac2e0032a6807a61dcc4cd7f9e5
Blockchain Developer <nicknewermore@gmail.com>
2023-07-11 19:43:43 +0300

    feat: add base universus warper

---
+++ contracts/universus/UniversusWarper.sol
@@ -0,0 +1,114 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import "@iqprotocol/iq-space-protocol/contracts/warper/ERC721/v1-controller/presets/ERC721ConfigurablePreset.sol";
+import "@iqprotocol/iq-space-protocol/contracts/warper/mechanics/v1-controller/renting-hook/IRentingHookMechanics.sol";
+import "@iqprotocol/iq-space-protocol/contracts/contract-registry/Contracts.sol";
+import "@iqprotocol/iq-space-protocol/contracts/renting/renting-manager/IRentingManager.sol";
+import "@iqprotocol/iq-space-protocol/contracts/listing/listing-terms-registry/IListingTermsRegistry.sol";
+import "@iqprotocol/iq-space-protocol/contracts/tax/tax-terms-registry/ITaxTermsRegistry.sol";
+
+import "./IUniversusWarper.sol";
+import "../auth/Auth.sol";
+
+/**
+ * @title Custom Warper for Universus
+ */
+contract UniversusWarper is IUniversusWarper, IRentingHookMechanics, ERC721ConfigurablePreset, Auth {
+    /**
+     * @dev ListingManager contact key.
+     */
+    bytes4 private immutable LISTING_MANAGER;
+
+    /**
+     * @dev RentingManager contact key.
+     */
+    bytes4 private immutable RENTING_MANAGER;
+
+    struct RentalDetails {
+        IListingTermsRegistry.ListingTerms listingTerms;
+        ITaxTermsRegistry.TaxTerms universeTaxTerms;
+        ITaxTermsRegistry.TaxTerms protocolTaxTerms;
+        uint256 rentalId;
+        uint256 listingId;
+    }
+
+    /**
+     * @dev renter address => tokenId => rentalId.
+     */
+    mapping(address => mapping(uint256 => uint256)) internal _lastActiveRental;
+
+    /**
+     * @dev rentalId => rentalDetails.
+     */
+    mapping(uint256 => RentalDetails) internal _rentalDetails;
+
+    /**
+     * @dev Returns last active rentalId for `renter` and `tokenId`.
+     * @param renter Renter.
+     * @param tokenId Token ID.
+     * @return Rental ID.
+     */
+    function getLastActiveRentalId(address renter, uint256 tokenId) external view returns (uint256) {
+        return _lastActiveRental[renter][tokenId];
+    }
+
+    /**
+     * @dev Returns RentalDetails for 'rentalId'.
+     * @param rentId Rent ID.
+     * @return RentalDetails.
+     */
+    function getRentalDetails(uint256 rentId) public view returns (RentalDetails) {
+        return _rentalDetails[rentId];
+    }
+
+    /**
+     * @dev Constructor for the IQNFTWarper contract.
+     */
+    constructor(bytes memory config) Auth() warperInitializer {
+        super.__initialize(config);
+
+        // (, , address rewardPool) = abi.decode(config, (address, address, address));
+        // setRewardPool(rewardPool);
+
+        LISTING_MANAGER = Contracts.LISTING_MANAGER;
+        RENTING_MANAGER = Contracts.RENTING_MANAGER;
+    }
+
+    /**
+     * @inheritdoc IRentingHookMechanics
+     */
+    function __onRent(
+        uint256 rentalId,
+        Rentings.Agreement calldata rentalAgreement,
+        Accounts.RentalEarnings calldata /* rentalEarnings */
+    ) external override onlyRentingManager returns (bool, string memory) {
+        // (, uint256 tokenId) = _tokenWithId(rentalAgreement.warpedAssets[0]);
+
+        for (uint256 i = 0; i < rentalAgreement.warpedAssets.length; i++) {
+            (, uint256 tokenId) = _decodeAssetId(rentalAgreement.warpedAssets[i].id);
+            // Latest active rental is persisted.
+            _lastActiveRental[rentalAgreement.renter][tokenId] = rentalId;
+
+            RentalDetails storage rentalDetails = _rentalDetails[rentalId];
+            rentalDetails.listingTerms = rentalAgreement.agreementTerms.listingTerms;
+            rentalDetails.universeTaxTerms = rentalAgreement.agreementTerms.universeTaxTerms;
+            rentalDetails.protocolTaxTerms = rentalAgreement.agreementTerms.protocolTaxTerms;
+            rentalDetails.rentalId = rentalId;
+            rentalDetails.listingId = rentalAgreement.listingId;
+        }
+
+        // Inform Renting Manager that everything is fine
+        return (true, "");
+    }
+
+    /**
+     * @inheritdoc IERC165
+     */
+    function supportsInterface(bytes4 interfaceId) public view virtual override returns (bool) {
+        return
+            interfaceId == type(IRentingHookMechanics).interfaceId ||
+            interfaceId == type(IUniversusWarper).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+}
